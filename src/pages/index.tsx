c0ac4ecbac9968c4da0332c438319ba245a8c364
Diego (D) <actual.mensch0r@icloud.com>
2023-01-09 17:59:25 -0600

    build: set up tailwindCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import CatCard from '../../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../../components/cards/cat/CatCard.mock';
 import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../../components/layouts/sidebar/SidebarLayout';
-import styles from '../styles/Home.module.css';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <CatCard {...mockCatCardProps.base} />
-      <CatCard {...mockCatCardProps.base} />
-      <CatCard {...mockCatCardProps.base} />
+    <section className="">
+      <h1 className="text-xl">Welcome to Electric World</h1>
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  // Ensure these match with .storybook/preview.js
   theme: {
-    extend: {},
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
   },
   plugins: [],
 };
