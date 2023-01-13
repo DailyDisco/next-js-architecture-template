c083e7109a90e68d399936a497087cb6b63d8920
Diego (D) <actual.mensch0r@icloud.com>
2023-01-13 10:46:33 -0600

    feat: implement tailwind css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,37 @@
+// import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
+// import SidebarLayout from '../../components/layouts/sidebar/SidebarLayout';
+// import { NextPageWithLayout } from './page';
+
+// const Home: NextPageWithLayout = () => {
+//   return (
+//     <section className="">
+//       <h1 className="text-xl">Welcome to Electric World</h1>
+//       <p className="text-sm"> This is the home page</p>
+//     </section>
+//   );
+// };
+
+// export default Home;
+
+// Home.getLayout = (page) => {
+//   return (
+//     <PrimaryLayout>
+//       <SidebarLayout />
+//       {page}
+//     </PrimaryLayout>
+//   );
+// };
+
 import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../../components/layouts/sidebar/SidebarLayout';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className="">
-      <h1 className="text-xl">Welcome to Electric World</h1>
+    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
     </section>
   );
 };
