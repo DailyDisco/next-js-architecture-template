8819449285374a175652d5a739adcc147b7db237
Diego (D) <actual.mensch0r@icloud.com>
2023-01-13 11:08:14 -0600

    feat: add base templates and start nav components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,3 @@
-// import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
-// import SidebarLayout from '../../components/layouts/sidebar/SidebarLayout';
-// import { NextPageWithLayout } from './page';
-
-// const Home: NextPageWithLayout = () => {
-//   return (
-//     <section className="">
-//       <h1 className="text-xl">Welcome to Electric World</h1>
-//       <p className="text-sm"> This is the home page</p>
-//     </section>
-//   );
-// };
-
-// export default Home;
-
-// Home.getLayout = (page) => {
-//   return (
-//     <PrimaryLayout>
-//       <SidebarLayout />
-//       {page}
-//     </PrimaryLayout>
-//   );
-// };
-
 import PrimaryLayout from '../../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../../components/layouts/sidebar/SidebarLayout';
 import { NextPageWithLayout } from './page';
