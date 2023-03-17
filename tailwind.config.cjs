d1f62a1900b6326b86db2c493cafee994d72dbc2
Diego (D) <actual.mensch0r@icloud.com>
2023-03-17 05:00:25 -0500

    fix: tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     // Ensure these match with .storybook/preview.js
     screens: {
