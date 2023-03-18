855caf028867ea53d12d6cb92571cb7f9dd7adc3
Diego (D) <actual.mensch0r@icloud.com>
2023-03-18 01:43:07 -0500

    fix: add warn rules to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,16 +20,16 @@ const config = {
   },
   plugins: ['@typescript-eslint'],
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-  // rules: {
-  //   '@typescript-eslint/consistent-type-imports': [
-  //     'warn',
-  //     {
-  //       prefer: 'type-imports',
-  //       fixStyle: 'inline-type-imports',
-  //     },
-  //   ],
-  //   '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  // },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    // '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
 };
 
 module.exports = config;
