e84cd532f43262b3c3a290f8859c7f93a4b4d24c
DailyDisco <actual.mensch0r@icloud.com>
2023-03-21 19:49:12 -0700

    feat: add GoogleProvider to [nexauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,17 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '~/server/auth';
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import GoogleProvider from 'next-auth/providers/google';
+
+export const AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    // add more providers here
+  ],
+};
+
 export default NextAuth(authOptions);
